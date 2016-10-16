bfef176add9abdd0e70f4e8499bb5655ec80814a
tones31 <anthagostino@gmail.com>
2016-10-16 16:26:09 -0400

    test
    added tests

---
+++ test/test.js
@@ -0,0 +1,74 @@
+"use strict";
+
+var expect = chai.expect;
+
+$(document).ready(function(){
+	var $body = $('body');
+
+
+	// addToSelect
+	$('#select').addToSelect({
+		a : 'option a',
+		b : 'option b',
+		c : 'option c'
+	}).addToSelect('d', 'option d');
+
+});
+
+
+// EventSystem
+
+describe("EventSystem", function() {
+	describe("create", function() {
+		it("should return a new event object", function() {
+			var t = new EventSystem();
+			var e = t._create('foo');
+			expect(e).to.have.key("callbacks");
+		});
+	});
+	describe("on", function() {
+		it("should add a new event if it does not exist", function() {
+			var t = new EventSystem();
+			function fooListener(sender, e){}
+			t.on('foo', fooListener);
+			expect(t.events).to.have.key("foo");
+		});
+		it("should add on to list of previous listeners", function() {
+			var t = new EventSystem();
+			function fooListener(sender, e){}
+			function fooListener2(sender, e){}
+			t.on('foo', fooListener);
+			t.on('foo', fooListener2);
+			expect(t.events.foo.callbacks.length).to.equal(2);
+		});
+		it("should add a new event callback if the event exists", function() {
+			var t = new EventSystem();
+			t._create('foo');
+			function fooListener(sender, e){}
+			t.on('foo', fooListener);
+			expect(t.events.foo.callbacks.length).to.equal(1);
+		});
+	});
+	describe("off", function() {
+		it("should remove an event listener if it exists", function() {
+			var t = new EventSystem();
+			function fooListener(sender, e){}
+			function fooListener2(sender, e){}
+			t.on('foo', fooListener);
+			t.on('foo', fooListener2);
+			t.off('foo', fooListener2);
+			expect(t.events.foo.callbacks.length).to.equal(1);
+		});
+	});
+	describe("trigger", function() {
+		it("should run all callbacks for an event", function() {
+			var t = new EventSystem();
+			var res = {};
+			t.on('foo', function(val){
+				res.e = val;
+			});
+			t.trigger('foo', 555);
+			expect(res.e).to.equal(555);
+		});
+	});
+});
